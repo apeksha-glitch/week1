b542f82ae9080f4845b6db565542743595253d5c
apeksha-glitch <desailg143@gmail.com>
2025-08-13 14:28:45 +0530

    Add mario3.c

---
+++ mario3.c
@@ -0,0 +1,14 @@
+//Print a 3 by 3 grid of bricks with a loop
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 3; i ++)
+    {
+        for (int j = 0 ; j <3 ; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
