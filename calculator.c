46d9df6cdbb38badc02ce31d2c0317bfed99593d
apeksha-glitch <desailg143@gmail.com>
2025-08-13 15:02:55 +0530

    Add calculator.c

---
+++ calculator.c
@@ -0,0 +1,18 @@
+//Addition with int 
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //Prompt user for x
+    int x = get_int("x:");
+
+    //Prompt user for y
+    int y = get_int("y:");
+
+    //Add x and y
+    int z = x + y;
+
+    //perform the addition
+    printf("%i\n", z);
+}
