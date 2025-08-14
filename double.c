3ff6a7c9e05f45b03ba93a473cde94fe649907c8
apeksha-glitch <desailg143@gmail.com>
2025-08-14 16:23:26 +0530

    Add double.c

---
+++ double.c
@@ -0,0 +1,22 @@
+//int
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int dollars = 1;
+    while (true)
+    {
+        char c= get_char("Hers's $%i.double it and give it to the next person: ", dollars);
+        if (c == 'y')
+        {
+            dollars *=2;
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("Hers's $%i.\n" , dollars);
+}
