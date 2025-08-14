8bbc5120d06d61ce8cc21d124415ac0dcf051628
apeksha-glitch <desailg143@gmail.com>
2025-08-14 16:35:53 +0530

    Add dollars.c

---
+++ dollars.c
@@ -0,0 +1,21 @@
+// long
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    long dollars = 1;
+    while(true)
+    {
+        char c =get_char("Here,s $%li. Double it and give it to the next person?", dollars);
+        if (c =='y')
+        {
+            dollars *=2;
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("Here's $%li.ln", dollars);
+}
